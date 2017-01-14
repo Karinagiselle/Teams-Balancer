2ee2b5cd8ea9327c7bacb29784ca3233c4819e28
Karina Blanco <karinagiselle.b@gmail.com>
2017-01-14 20:01:13 -0300

    Agrega entidad Jugador

---
+++ app/src/main/java/com/teamsbalancer/kari/teamsbalancer/entidades/Jugador.java
@@ -0,0 +1,17 @@
+package com.teamsbalancer.kari.teamsbalancer.entidades;
+
+/**
+ * Created by kari on 14/01/17.
+ */
+
+public class Jugador {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ app/src/test/java/com/teamsbalancer/kari/teamsbalancer/JugadorTest.java
@@ -0,0 +1,21 @@
+package com.teamsbalancer.kari.teamsbalancer;
+
+import com.teamsbalancer.kari.teamsbalancer.entidades.Jugador;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class JugadorTest {
+
+    public static final String NOMBRE = "Michael Jordan";
+
+    @Test
+    public void crearJugadorConNombre() throws Exception {
+        Jugador jugador = new Jugador();
+        jugador.setNombre(NOMBRE);
+
+        assertThat(jugador.getNombre(), equalTo(NOMBRE));
+    }
+}
